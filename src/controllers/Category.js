a8bda91963da8dd7174190bd43db730916509732
thinhnv789 <thinhnv.vnu@gmail.com>
2017-09-28 23:31:15 +0700

    abc

--- src/controllers/Category.js
+++ src/controllers/Category.js
@@ -28,8 +28,6 @@ exports.getCreate = function (req, res) {
 			console.log('err', err)
 			return done(err);
 		}
-
-		console.log(parents[0].children[0].children[0])
 		
 		res.render('category/create', {
 			title: 'Create New Category',
@@ -37,9 +35,6 @@ exports.getCreate = function (req, res) {
 			parents: parents
 		});
 	});
-	// res.render('category/create', {
-	//     title: 'Create New Category'
-	// });
 };
 
 exports.postCreate = function (req, res) {
@@ -108,6 +103,110 @@ exports.postCreate = function (req, res) {
 			}
 		});
 	});
+};
+
+/**
+ * Get update category
+ */
+exports.getUpdate = function(req, res) {
+	var categoryId = req.params.categoryId;
+
+	Category.findOne({_id: categoryId}).exec(function (err, category) {
+		if (err) {
+			console.log('err', err)
+			return done(err);
+		}
+		
+		// Find all category for select parent
+		Category.find({parent: null, _id: { $ne: categoryId }}).populate('children').populate({
+			path: 'children',
+			populate: { path: 'children', model: Category } // <--- specify the model explicitly
+		}).exec(function (err, parents) {
+			if (err) {
+				console.log('err', err)
+				return done(err);
+			}
+			console.log('category', category);
+			// Render view
+			res.render('category/update', {
+				title: 'Update Category',
+				current: ['category', 'update'],
+				data: category,
+				parents: parents
+			});
+		});
+	});
+}
+
+/**
+ * Post update category
+ */
+exports.postUpdate = function (req, res) {
+	var categoryId = req.params.categoryId, data = req.body;
+	/*
+	* Validate create category
+	*/ 
+	req.checkBody('categoryName', 'Bạn vừa update tên danh mục trống').notEmpty();
+	Category.findOne({_id: categoryId}).exec(function (err, category) {
+		var errors = req.getValidationResult().then(function(errors) {
+			if (!errors.isEmpty()) {
+				var errors = errors.mapped();
+				//If there are errors render the form again, passing the previously entered values and errors
+				Category.find({parent: null}).populate('children').populate({
+					path: 'children',
+					populate: { path: 'children', model: Category }
+				}).exec(function (err, parents) {
+					if (err) {
+						console.log('err', err)
+						return done(err);
+					}
+					console.log('data', data);
+					res.render('category/update', {
+						title: 'Create New Category',
+						current: ['category', 'create'],
+						parents: parents,
+						data: category,
+						errors: errors
+					});
+				});
+				return;
+			}
+
+			/*
+			* End validate
+			*/
+			category.categoryName = data.categoryName;
+			category.alias = data.alias;
+			category.icon = '';
+			category.parent = data.parentId ? data.parentId : null;
+			category.status = data.status;
+			// save the user
+			category.save(function (err) {
+				if (err) {
+					console.log('Error in Saving: ' + err);
+					res.send({ "result": false });
+				}
+				// Insert child to category
+				if (category.parent) {
+					Category.findOne({ _id: category.parent }, function (err, cat) {
+						if (!err) {
+							cat.children.push(category._id);
+							cat.save(function (er) {
+								if (err) {
+									console.log('Error in Saving children: ' + err);
+									res.send({ "result": false });
+								} else {
+									res.redirect('/category');
+								}	
+							})
+						}
+					})
+				} else {
+					res.redirect('/category');
+				}
+			});
+		});
+	})
 };
 
 exports.deleteCategory = function (req, res) {
--- src/routes/category.js
+++ src/routes/category.js
@@ -6,5 +6,7 @@ var CategoryController = require('../controllers/Category');
 router.get('/', CategoryController.getIndex);
 router.get('/create', CategoryController.getCreate);
 router.post('/create', CategoryController.postCreate);
+router.get('/update/:categoryId', CategoryController.getUpdate);
+router.post('/update/:categoryId', CategoryController.postUpdate);
 
 module.exports = router;
